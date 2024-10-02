3f3b71dce0531b66eb56013f33fea8041322ec9e
BreakBB <b.boeing@online.de>
2024-10-02 07:28:00 +0200

    Uninstall @mui as this is a pure @emotion issue

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {Typography} from '@mui/material';
+import styled from "@emotion/styled";
+
+const Typography = styled.p({
+    color: "hotpink",
+});
 
 function App() {
   const [count, setCount] = useState(0)
